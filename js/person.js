9aec1da6d661f4ad3cddf1fa509e2dfb212aa5c6
szabozsolt20 <szabo.zsolt.debrecen@gmail.com>
2021-10-29 22:35:09 +0200

    Hibásan fut le a tesztem a summation és a subtraction fv-ekre. - 2.

--- js/person.js
+++ js/person.js
@@ -2,6 +2,6 @@ const personDataLog = ({
     firstname = "John",
     lastname = "Doe",
     age = 33
-} = {}) => {
+} = {firstname,lastname,age}) => {
     `My name is ${firstname} ${lastname}. I'm ${age} years old.`;
 };
