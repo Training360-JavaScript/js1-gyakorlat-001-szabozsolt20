ff3f6af8be772b5be1a3034ae2cc92d2c252e04c
szabozsolt20 <szabo.zsolt.debrecen@gmail.com>
2021-10-29 22:57:09 +0200

    Na, most már jó (return-ök hiányoztak....

--- js/person.js
+++ js/person.js
@@ -2,6 +2,10 @@ const personDataLog = ({
     firstname = "John",
     lastname = "Doe",
     age = 33
-} = {firstname,lastname,age}) => {
-    `My name is ${firstname} ${lastname}. I'm ${age} years old.`;
+} = {
+    firstname,
+    lastname,
+    age
+}) => {
+   return `My name is ${firstname} ${lastname}. I'm ${age} years old.`;
 };
--- js/sum.js
+++ js/sum.js
@@ -1,2 +1,2 @@
-const summation = (a = 0, b = 0) => {a + b};
-const subtraction = (a = 0, b = 0) => {a - b};
+const summation = (a = 0, b = 0) => {return a + b};
+const subtraction = (a = 0, b = 0) => {return a - b};
